d4ddd11ccca9b359831ef53f1f9f42460bff219f
Prakash8782 <prakash8782@gmail.com>
2022-09-19 14:29:33 +0530

    fill

---
+++ fill/main.js
@@ -0,0 +1,7 @@
+let colors=["Red","Blue","Green","Yellow","Pink"];
+console.log("colors : " ,colors);
+colors.fill("Black",3,4);
+console.log("length of an array : "+colors.length);
+console.log("method : array.fill()");
+console.log("change in array : ",colors);
+console.log("return : length of array after fill(); is "+colors.length);
